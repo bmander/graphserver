c372df37a1550225bfd42319eb1c81bb9e379f87
ewedistrict <ewedistrict@5e9d6a62-aa2b-0410-a6b7-c1830f73770d>
2007-07-11 00:43:22 +0000

    Added a few comments

--- install.rb
+++ install.rb
@@ -1,8 +1,10 @@
 script_install_dir = $:[-2]   #location of the standard library
 
+# Create ruby extension Makefile
 Dir.chdir( "core" )
 require 'extconf.rb'
 
+# Modify ruby extension Makefile
 makefile = File.read "Makefile"
 makefile.gsub!( "edgeweights.o", "" )
 makefile.gsub!( "edgeweights.c", "" )
@@ -10,10 +12,12 @@ makefile.gsub!( "router.o", "" )
 makefile.gsub!( "router.c", "" )
 makefile << "\nme_a_sandwich:\n\techo okay\n"
 
+# Write modified ruby extension Makefile
 fp = File.new( "Makefile", "w" )
 fp << makefile
 fp.close
 
+# Execute modified ruby extension Makefile, and install resulting files
 print `make clean`
 print `make install`
 print "installing to: #{script_install_dir}\n"
