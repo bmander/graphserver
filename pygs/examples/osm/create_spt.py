580a0c0b1e5e25e7309bd7bbdc7c97368edf7716
ewedistrict <ewedistrict@5e9d6a62-aa2b-0410-a6b7-c1830f73770d>
2008-07-04 00:00:25 +0000

    split out an osm library from the osm example

---
+++ pygs/examples/osm/create_spt.py
@@ -0,0 +1,49 @@
+import time
+from osm import OSM, Node, Way
+import sys
+sys.path.append('../..')
+from structures import Graph, Street, State
+
+
+from urllib import urlopen
+def get_osm_xml( left, bottom, right, top ):
+    fp = urlopen( "http://api.openstreetmap.org/api/0.5/map?bbox=%f,%f,%f,%f"%(left,bottom,right,top) )
+    return fp.read()
+
+def main():
+    #print get_osm_xml( -122.33, 47.66, -122.31, 47.68 )
+
+    osmdata = open("map.osm").read()
+            
+    print "read osm file"
+    osm = OSM(osmdata)
+
+    print "load vertices into graph file"
+    g = Graph()
+    for nodeid in osm.nodes.keys():
+        g.add_vertex( str(nodeid) )
+
+    print "load edges into graph file"
+    for wayid, way in osm.ways.iteritems():
+        if 'highway' in way.tags:
+            g.add_edge( str(way.fromv), str(way.tov), Street( wayid, way.length ) )
+            g.add_edge( str(way.tov), str(way.fromv), Street( wayid, way.length ) )
+        
+    random_vertex_label = "53122137"
+    
+    print "find shortest path tree"
+    t0 = time.time()
+    spt = g.shortest_path_tree( random_vertex_label, "bogus", State(0) )
+    t1 = time.time()
+    print "took: %f"%(t1-t0)
+    
+    fp = open("points.txt", "w")
+    for edge in spt.edges:
+        weight = edge.to_v.payload.weight
+        points = osm.ways[ edge.payload.name ].get_projected_points()
+        
+        fp.write( "%d:"%weight+",".join( [" ".join([str(c) for c in p]) for p in points] ) + "\n" )
+    fp.close()
+        
+if __name__=='__main__':
+    main()
--- pygs/examples/osm/osm.py
+++ pygs/examples/osm/osm.py
@@ -1,17 +1,6 @@
 from xml.dom.minidom import parseString
-
-import sys
-sys.path.append('..')
-from structures import Graph, Street, State
-
-from urllib import urlopen
-
-
-def get_osm_xml( left, bottom, right, top ):
-    fp = urlopen( "http://api.openstreetmap.org/api/0.5/map?bbox=%f,%f,%f,%f"%(left,bottom,right,top) )
-    return fp.read()
-    
-
+import copy
+from pyproj import Proj, transform
 
 class Node:
     def __init__(self, data):
@@ -28,11 +17,9 @@ class Node:
     def __repr__(self):
         return "<Node lon=%f lat=%f>"%(self.lon, self.lat)
 
-import copy
-from pyproj import Proj, transform
-
 def dist(x1,y1,x2,y2):
     return ((x2-x1)**2+(y2-y1)**2)**0.5
+    
 utmzone10 = Proj(init='epsg:26910')
 
 class Way:
@@ -137,44 +124,3 @@ class OSM:
             for split_way in split_ways:
                 new_ways[split_way.id] = split_way
         self.ways = new_ways
-            
-        
-import time
-def main():
-    #print get_osm_xml( -122.33, 47.66, -122.31, 47.68 )
-
-    osmdata = open("map.osm").read()
-            
-    print "read osm file"
-    osm = OSM(osmdata)
-
-    print "load vertices into graph file"
-    g = Graph()
-    for nodeid in osm.nodes.keys():
-        g.add_vertex( str(nodeid) )
-
-    print "load edges into graph file"
-    for wayid, way in osm.ways.iteritems():
-        if 'highway' in way.tags:
-            g.add_edge( str(way.fromv), str(way.tov), Street( wayid, way.length ) )
-            g.add_edge( str(way.tov), str(way.fromv), Street( wayid, way.length ) )
-        
-    random_vertex_label = "53122137"
-    
-    print "find shortest path tree"
-    t0 = time.time()
-    spt = g.shortest_path_tree( random_vertex_label, "bogus", State(0) )
-    t1 = time.time()
-    print "took: %f"%(t1-t0)
-    
-    fp = open("points.txt", "w")
-    for edge in spt.edges:
-        weight = edge.to_v.payload.weight
-        points = osm.ways[ edge.payload.name ].get_projected_points()
-        
-        fp.write( "%d:"%weight+",".join( [" ".join([str(c) for c in p]) for p in points] ) + "\n" )
-    fp.close()
-        
-if __name__=='__main__':
-    main()
-    
