7783951b55c9ca5ba1b26d5a22f4a3d4ae1a4ed7
ewedistrict <ewedistrict@5e9d6a62-aa2b-0410-a6b7-c1830f73770d>
2008-07-07 20:05:13 +0000

    reimplemented import to use streaming XML parser

--- pygs/examples/osm/osm.py
+++ pygs/examples/osm/osm.py
@@ -1,42 +1,25 @@
-from xml.dom.minidom import parseString
+import xml.sax
 import copy
 from pyproj import Proj, transform
-
-class Node:
-    def __init__(self, data):
-        self.id  = int( data.attributes['id'].nodeValue )
-        self.lat = float( data.attributes['lat'].nodeValue )
-        self.lon = float( data.attributes['lon'].nodeValue )
-        
-        self.tags = {}
-        for tag in data.getElementsByTagName("tag"):
-            k = tag.attributes['k'].nodeValue
-            v = tag.attributes['v'].nodeValue
-            self.tags[k] = v
-            
-    def __repr__(self):
-        return "<Node lon=%f lat=%f>"%(self.lon, self.lat)
 
 def dist(x1,y1,x2,y2):
     return ((x2-x1)**2+(y2-y1)**2)**0.5
     
 utmzone10 = Proj(init='epsg:26910')
 
-class Way:
-    def __init__(self, data, osm):
-        self.osm = osm
-        self.id = data.attributes['id'].nodeValue
-        
-        self.nodes = []
-        for nd in data.getElementsByTagName("nd"):
-            self.nodes.append( int( nd.attributes['ref'].nodeValue ) )
-            
+class Node:
+    def __init__(self, id, lon, lat):
+        self.id = id
+        self.lon = lon
+        self.lat = lat
         self.tags = {}
-        for tag in data.getElementsByTagName("tag"):
-            k = tag.attributes['k'].nodeValue
-            v = tag.attributes['v'].nodeValue
-            self.tags[k] = v
-    
+        
+class Way:
+    def __init__(self, id):
+        self.id = id
+        self.nds = []
+        self.tags = {}
+        
     def split(self, dividers):
         # slice the node-array using this nifty recursive function
         def slice_array(ar, dividers):
@@ -51,7 +34,7 @@ class Way:
                     return [left]+rightsliced
             return [ar]
             
-        slices = slice_array(self.nodes, dividers)
+        slices = slice_array(self.nds, dividers)
         
         # create a way object for each node-array slice
         ret = []
@@ -59,27 +42,28 @@ class Way:
         for slice in slices:
             littleway = copy.copy( self )
             littleway.id += "-%d"%i
-            littleway.nodes = slice
+            littleway.nds = slice
             ret.append( littleway )
             i += 1
             
         return ret
         
-    def get_projected_points(self ):
+    def get_projected_points(self, nodedir):
+        """nodedir is a dictionary of nodeid->node objects"""
         ret = []
-        for nodeid in self.nodes:
-            node = self.osm.nodes[ nodeid ]
+        for nodeid in self.nds:
+            node = nodedir[ nodeid ]
             ret.append( utmzone10(node.lon,node.lat) )
             
         return ret
-    
-    @property
-    def length(self):
+        
+    def length(self, nodedir):
+        """nodedir is a dictionary of nodeid->node objects"""
         ret = 0
         
-        for i in range(len(self.nodes)-1):
-            thisnode = self.osm.nodes[ self.nodes[i] ]
-            nextnode = self.osm.nodes[ self.nodes[i+1] ]
+        for i in range(len(self.nds)-1):
+            thisnode = nodedir[ self.nds[i] ]
+            nextnode = nodedir[ self.nds[i+1] ]
             
             fromx, fromy = utmzone10(thisnode.lon,thisnode.lat)
             tox, toy = utmzone10(nextnode.lon,nextnode.lat)
@@ -90,31 +74,61 @@ class Way:
         
     @property
     def fromv(self):
-        return self.nodes[0]
+        return self.nds[0]
         
     @property
     def tov(self):
-        return self.nodes[-1]
+        return self.nds[-1]
 
 class OSM:
-    def __init__(self, data):
-        self.nodes = {}
-        self.ways = {}
+    def __init__(self, filename):
+        nodes = {}
+        ways = {}
         
-        dom = parseString( data )
+        class OSMHandler(xml.sax.ContentHandler):
+            @classmethod
+            def setDocumentLocator(self,loc):
+                pass
+            
+            @classmethod
+            def startDocument(self):
+                pass
+                
+            @classmethod
+            def endDocument(self):
+                pass
+                
+            @classmethod
+            def startElement(self, name, attrs):
+                if name=='node':
+                    self.currElem = Node(attrs['id'], float(attrs['lon']), float(attrs['lat']))
+                elif name=='way':
+                    self.currElem = Way(attrs['id'])
+                elif name=='tag':
+                    self.currElem.tags[attrs['k']] = attrs['v']
+                elif name=='nd':
+                    self.currElem.nds.append( attrs['ref'] )
+                
+            @classmethod
+            def endElement(self,name):
+                if name=='node':
+                    nodes[self.currElem.id] = self.currElem
+                elif name=='way':
+                    ways[self.currElem.id] = self.currElem
+                
+            @classmethod
+            def characters(self, chars):
+                pass
+
+        xml.sax.parse(filename, OSMHandler)
         
-        for node in dom.getElementsByTagName("node"):
-            self.nodes[ int( node.attributes['id'].nodeValue ) ] = Node(node)
-            
-        for way in dom.getElementsByTagName("way"):
-            self.ways[ way.attributes['id'].nodeValue ] = Way(way, self)
-            
-        del( dom )
+        self.nodes = nodes
+        self.ways = ways
             
         #count times each node is used
         node_histogram = dict.fromkeys( self.nodes.keys(), 0 )
         for way in self.ways.values():
-            for node in way.nodes:
+            for node in way.nds:
                 node_histogram[node] += 1
         
         #use that histogram to split all ways, replacing the member set of ways
@@ -124,3 +138,8 @@ class OSM:
             for split_way in split_ways:
                 new_ways[split_way.id] = split_way
         self.ways = new_ways
+    
+if __name__=='__main__':
+    osm = OSM("map.osm")
+    for way in osm.ways.values():
+        way.length(osm.nodes)
