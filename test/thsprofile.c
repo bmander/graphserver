5bbbdcbf5ff9aa60b76ccd15f6f47e861827c384
ewedistrict <ewedistrict@5e9d6a62-aa2b-0410-a6b7-c1830f73770d>
2008-07-10 21:38:41 +0000

    leak check test case for graph made of triphopschedules

---
+++ test/thsprofile.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../core/graph.h"
+#include <valgrind/callgrind.h>
+
+int main() {
+    
+    Graph* gg = gNew(); 
+    
+    FILE* fp = fopen("transit.txt", "r");
+    
+    // Load up the calendar
+    int ncaldays;
+    fscanf( fp, "%d\n", &ncaldays );
+
+    //printf( "%d\n", ncaldays );
+    
+    CalendarDay* cal = NULL;
+    int i;
+    for(i=0; i<ncaldays; i++) {
+        long daystart;
+        long dayend;
+        int nserviceids;
+        int daylightsavings;
+        fscanf( fp, "%ld %ld %d %d\n", &daystart, &dayend, &nserviceids, &daylightsavings );
+        //printf( "%ld %ld %d\n", daystart, dayend, nserviceids );
+        
+        int serviceids[nserviceids];
+        int j;
+        for(j=0; j<nserviceids; j++) {
+            int service_id;
+            fscanf( fp, "%d\n", &service_id );
+            serviceids[j]=service_id;
+            //printf( "%d\n", service_id);
+        }
+        
+        cal = calAppendDay( cal, daystart, dayend, nserviceids, serviceids, daylightsavings );
+    }
+    cal = calRewind(cal);
+    
+    //Load up vertices
+    int nvertices;
+    fscanf( fp, "%d\n", &nvertices );
+    //printf( "%d\n", nvertices );
+    
+    for(i=0; i<nvertices; i++) {
+        char label[40];
+        fscanf( fp, "%s\n", &label );
+        //printf( "%s\n", label );
+        gAddVertex(gg, label );
+    }
+    
+
+    
+    //Load up triphopschedules
+    //for each vertex
+    for(i=0; i<nvertices; i++) {
+        char label[40];
+        int noutgoing;
+        fscanf( fp, "%s %d\n", &label, &noutgoing );
+        //printf( "%s %d\n", label, noutgoing );
+        
+        //for each outgoing edge
+        int j;
+        for(j=0; j<noutgoing; j++) {
+            char destlabel[40];
+            int service_id;
+            int timezone_offset;
+            int nhops;
+            
+            fscanf( fp, "%s %d %d %d\n", &destlabel, &service_id, &timezone_offset, &nhops );
+            //printf( "%s %d %d %d\n", destlabel, service_id, timezone_offset, nhops );
+            
+            int departs[nhops];
+            int arrives[nhops];
+            char* trip_ids[nhops];
+            
+            //for each triphop
+            int k;
+            for(k=0; k<nhops; k++) {
+                int depart;
+                int arrive;
+                char trip_id[40];
+                
+                fscanf( fp, "%d %d %s\n", &depart, &arrive, &trip_id );
+                //printf( "%d %d %s\n", depart, arrive, trip_id );
+                
+                departs[k] = depart;
+                arrives[k] = arrive;
+                trip_ids[k] = trip_id;
+            }
+            
+            TripHopSchedule* ths = thsNew( departs, arrives, trip_ids, nhops, service_id, cal, timezone_offset );
+            
+            gAddEdge( gg, label, destlabel, (EdgePayload*)ths );
+            
+        }
+    }
+    
+    fclose( fp );
+    
+    
+    long t = 1215674100; //#Wed 2008-7-9 11:15:00 PST-0800
+    Graph* spt = gShortestPathTree( gg, "16TH", "bogus", stateNew( t ) );
+    
+    gDestroy( spt, 1, 0 );
+    
+    gDestroy(gg, 1, 1);
+
+    calDestroy( cal );
+    
+    
+    
+    return 1;
+} 
