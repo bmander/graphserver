64650ad9a883e142e7273d20feb7c200460dd63b
ewedistrict <ewedistrict@5e9d6a62-aa2b-0410-a6b7-c1830f73770d>
2008-07-16 23:32:43 +0000

    updated examples to use the newly refactored ext tree

--- pygs/examples/osm/create_spt.py
+++ pygs/examples/osm/create_spt.py
@@ -1,43 +1,24 @@
+"""Here's a little example whereby a shortest path tree is generated for the San Francisco area starting at the node "65287655",
+   which corresponds to an intersection near the Presidio. The output is a text file where each line represents a branch in the 
+   shortest path tree. The line takes the format STARTING_VERTEX:ENDING_VERTEX:LENGTH:x1 y1,x2 y2..."""
+   
+   
 import time
-from pygs.ext.osm import OSM, Node, Way
-import sys
-sys.path.append('../..')
-from graphserver import Graph, Street, State
+from pygs.ext.osm.osm import OSM, Node, Way
+from pygs.graphserver import Graph, Street, State
+from pygs.ext.osm.load_osm import add_osm_to_graph
 from pyproj import Proj
-
-from urllib import urlopen
-def get_osm_xml( left, bottom, right, top ):
-    fp = urlopen( "http://api.openstreetmap.org/api/0.5/map?bbox=%f,%f,%f,%f"%(left,bottom,right,top) )
-    return fp.read()
 
 def main():
     #print get_osm_xml( -122.33, 47.66, -122.31, 47.68 )
     utmzone10 = Proj(init='epsg:26910')
  
-    print "read osm file"
-    osm = OSM("map.osm")
-
-    print "load vertices into graph file"
     g = Graph()
-    for nodeid in osm.nodes.keys():
-        g.add_vertex( str(nodeid) )
-
-    print "load edges into graph file"
-    for wayid, way in osm.ways.iteritems():
-        if 'highway' in way.tags:
-            len = way.length(osm.nodes, utmzone10)
-            
-            if way.tags['highway']=='cycleway':
-                len = len/3
-            elif way.tags['highway']=='motorway':
-                len = len*100
-            elif way.tags['highway']=='footway':
-                len = len/2
-            
-            g.add_edge( str(way.fromv), str(way.tov), Street( wayid, len ) )
-            g.add_edge( str(way.tov), str(way.fromv), Street( wayid, len ) )
+    osm = OSM("sf.osm")
+    
+    add_osm_to_graph(g, osm, utmzone10, {'cycleway':0.3333, 'footway':0.5, 'motorway':100} )
         
-    random_vertex_label = "53217079"
+    random_vertex_label = "65287655" #one end of Pitt Ave in Sebastapol
     
     print "find shortest path tree"
     t0 = time.time()
