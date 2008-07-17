61ca21495307a746d6617479b650c6701e17abd2
ewedistrict <ewedistrict@5e9d6a62-aa2b-0410-a6b7-c1830f73770d>
2008-07-17 00:51:34 +0000

    prefix all vertex labels with parameterized string when adding to graphserver graph

--- pygs/ext/osm/load_osm.py
+++ pygs/ext/osm/load_osm.py
@@ -1,13 +1,13 @@
 from osm import OSM,Node,Way
 from pygs.graphserver import Graph, Street
 
-def add_osm_to_graph(g, osm, projection, multipliers=[]):
+def add_osm_to_graph(g, osm, projection, multipliers=[], prefix="osm"):
     """multipliers is a dict of highway types to edge weight multipliers (highwaytype,multiplier) which effect the
        preferential weighting of a kind of edge. For example, {'cycleway':0.333} makes cycleways three
        times easier to traverse"""
     
     for nodeid in osm.nodes.keys():
-        g.add_vertex( str(nodeid) )
+        g.add_vertex( prefix+str(nodeid) )
     
     for wayid, way in osm.ways.iteritems():
         if 'highway' in way.tags:
@@ -16,6 +16,6 @@ def add_osm_to_graph(g, osm, projection, multipliers=[]):
             if way.tags['highway'] in multipliers:
                 len = len*multipliers[way.tags['highway']]
             
-            g.add_edge( str(way.fromv), str(way.tov), Street( wayid, len ) )
-            g.add_edge( str(way.tov), str(way.fromv), Street( wayid, len ) )
+            g.add_edge( prefix+str(way.fromv), prefix+str(way.tov), Street( wayid, len ) )
+            g.add_edge( prefix+str(way.tov), prefix+str(way.fromv), Street( wayid, len ) )
             
