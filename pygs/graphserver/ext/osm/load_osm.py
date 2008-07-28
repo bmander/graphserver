1d3ca09988fc4592da1d1f89dde3aa9f60ae1b64
ewedistrict <ewedistrict@5e9d6a62-aa2b-0410-a6b7-c1830f73770d>
2008-07-28 01:32:14 +0000

    load_osm takes filename or OSM object

--- pygs/graphserver/ext/osm/load_osm.py
+++ pygs/graphserver/ext/osm/load_osm.py
@@ -4,10 +4,15 @@ sys.path.append('../../..')
 from graphserver.core import Graph, Street
 
 class OSMLoadable:
-    def load_osm(self, osm, projection, multipliers=[], prefix="osm"):
+    def load_osm(self, osm_filename_or_object, projection, multipliers=[], prefix="osm"):
         """multipliers is a dict of highway types to edge weight multipliers (highwaytype,multiplier) which effect the
            preferential weighting of a kind of edge. For example, {'cycleway':0.333} makes cycleways three
            times easier to traverse"""
+           
+        if type(osm_filename_or_object) == str:
+            osm = OSM(osm_filename_or_object)
+        else:
+            osm = osm_filename_or_object
         
         for nodeid in osm.nodes.keys():
             self.add_vertex( prefix+str(nodeid) )
