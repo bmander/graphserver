d0a110254944de436e56ce9683b5e6614b4edae7
Brandon Martin-Anderson <badhill@gmail.com>
2009-02-06 11:57:55 -0800

    Now you can click on vertex's outgoing edge to see the output of the expound() method of that edge

--- apps/graphcrawler/graphcrawler.py
+++ apps/graphcrawler/graphcrawler.py
@@ -22,7 +22,7 @@ class GraphCrawler(Servable):
         for vertex1, vertex2, edgetype in self.graphdb.all_incoming( label ):
             ret.append( "<a href=\"/vertex?label=&quot;%s&quot;\">%s</a><pre>&nbsp;&nbsp;&nbsp;%s</pre>"%(vertex1, vertex1, cgi.escape(repr(edgetype))) )
         ret.append( "<h3>outgoing to:</h3>" )
-        for vertex1, vertex2, edgetype in self.graphdb.all_outgoing( label ):
+        for i, (vertex1, vertex2, edgetype) in enumerate( self.graphdb.all_outgoing( label ) ):
             s0 = State(1,int(currtime))
             s1 = edgetype.walk( s0 )
             
@@ -31,13 +31,20 @@ class GraphCrawler(Servable):
             else:
                 toterm = "<a href=\"/vertex?label=&quot;%s&quot;\">%s</a>"%(vertex2, vertex2)
             
-            ret.append( "%s<br><pre>&nbsp;&nbsp;&nbsp;via %s</pre>"%(toterm, cgi.escape(repr(edgetype))) )
+            ret.append( "%s<br><pre>&nbsp;&nbsp;&nbsp;via %s (<a href=\"/outgoing?label=&quot;%s&quot;&edgenum=%d\">details</a>)</pre>"%(toterm, cgi.escape(repr(edgetype)), vertex1, i) )
             
             if s1:
                 ret.append( "<pre>&nbsp;&nbsp;&nbsp;%s</pre>"%cgi.escape(str(s1)) )
                 
         return "".join(ret)
     vertex.mime = "text/html"
+    
+    def outgoing(self, label, edgenum):
+        all_outgoing = list( self.graphdb.all_outgoing( label ) )
+        
+        fromv, tov, edge = all_outgoing[edgenum]
+        
+        return edge.expound()
     
     def str(self):
         return str(self.graphdb)
@@ -46,5 +53,8 @@ if __name__ == '__main__':
     # a fine example node for bart: "ASBY" @ 1233172800
     # for trimet: "10071" @ 1233172800
     
-    gc = GraphCrawler("../package_graph/bartheadway.db")
+    GDB_FILENAME = "../package_graph/bartheadway.db"
+    GDB_FILENAME = "/home/brandon/urbanmapping/transit_routing/router/data/bart.gdb"
+    
+    gc = GraphCrawler(GDB_FILENAME)
     gc.run_test_server()
