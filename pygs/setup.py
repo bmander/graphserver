f789715385d1030f7296992b32cb7a5005c029ad
bmander <badhill@gmail.com>
2009-02-24 14:07:59 -0700

    cleaned up whitespace

--- pygs/setup.py
+++ pygs/setup.py
@@ -1,57 +1,29 @@
 from distutils.core import setup
-
 import os.path, sys
-
-
 
 LIBSO = '../core/libgraphserver.so'
 
-
-
 if not os.path.exists(LIBSO):
-
     print "ERROR: %s not found.  Have you run '(cd core && make)' yet?" % LIBSO
-
     sys.exit(-1)
 
-
-
-"""py_modules=['graphserver.gsdll', 'graphserver.core', 'graphserver.engine', 'graphserver.util', 'graphserver.ext', 'graphserver.ext.osm', 'graphserver.ext.gtfs', 'graphserver.ext.gtfs.load_gtfs',
-
-                   'graphserver.ext.osm.osm', 'graphserver.ext.osm.graph', 'graphserver.ext.osm.engine', 'graphserver.ext.osm.load_osm'],"""
+"""py_modules=['graphserver.gsdll', 'graphserver.core', 'graphserver.engine', 'graphserver.util', 'graphserver.ext', 'graphserver.ext.osm', 'graphserver.ext.gtfs', 'graphserver.ext.gtfs.load_gtfs', 'graphserver.ext.osm.osm', 'graphserver.ext.osm.graph', 'graphserver.ext.osm.engine', 'graphserver.ext.osm.load_osm'],"""
 
 setup( name='graphserver',
-
        version='0.1',
-
        url='http://graphserver.wiki.sourceforge.net/',
-
        packages = ['graphserver.ext'],
-
        py_modules=['graphserver.gsdll', 
-
                    'graphserver.core', 
-
                    'graphserver.engine', 
-
                    'graphserver.util',
-
                    'graphserver.graphdb',
-
                    'graphserver.ext.gtfs.load_gtfs',
-
                    'graphserver.ext.gtfs.gtfsdb',
-
                    'graphserver.ext.osm.osm', 
-
                    'graphserver.ext.osm.vincenty',
-
                    'graphserver.ext.osm.graph', 
-
                    'graphserver.ext.osm.engine', 
-
                    'graphserver.ext.osm.load_osm',
-
                    'graphserver.ext.osm.osmdb',],
-
        data_files=[('/usr/lib',[LIBSO])])
