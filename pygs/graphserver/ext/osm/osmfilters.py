e19c52805a60bf39bfdcb5a0bc88c97bddfd31c5
Nino Walker <kolohe@lscape.local>
2009-03-16 04:36:34 +0800

    Added some useful filters for selectively purging ways and nodes.
    Signed-off-by: Brandon Martin-Anderson <badhill@gmail.com>

--- pygs/graphserver/ext/osm/osmfilters.py
+++ pygs/graphserver/ext/osm/osmfilters.py
@@ -3,28 +3,102 @@ from osmdb import OSMDB
 import time
 
 class OSMDBFilter(object):
-    def setup(self, db):
+    def setup(self, db, *args):
         pass
     
-    def filter(self, db):
+    def filter(self, db, *args):
         pass
     
     def teardown(self, db):
         pass
     
-    def run(self,db):
-        self.setup(db)
-        self.filter(db)
+    def run(self,db, *args):
+        self.setup(db, *args)
+        self.filter(db, *args)
 
-    def rerun(self,db):
+    def rerun(self,db, *args):
         self.teardown(db)
         self.run(db)
         
-    def visualize(self, db):
+    def visualize(self, db, *args):
         pass
-    
+
+class DeleteHighwayTypesFilter(OSMDBFilter):
+    def run(self, db, *types):
+        print "Types",types
+        purge_list = []
+        for way in db.ways():
+            if 'highway' in way.tags and way.tags['highway'] in types:
+                purge_list.append(way.id)
+        
+        c = db.cursor()
+        for i in range(0,len(purge_list),100):
+            query = "DELETE from ways WHERE id in ('%s')" % "','".join(purge_list[i:i+100])
+            c.execute(query)
+        db.conn.commit()
+        c.close()
+        print "Deleted all %s highway types (%s ways)" % (", ".join(types), len(purge_list))
+        DeleteOrphanNodesFilter().run(db,*args)
+
+        
+class DeleteOrphanNodesFilter(OSMDBFilter):
+    def run(self, db, *args):
+        node_ids = {}
+        for nid in db.execute("SELECT id from nodes"):
+            node_ids[nid[0]] = 0
+        
+        for way in db.ways():
+            node_ids[way.nds[0]] += 1
+            node_ids[way.nds[-1]] += 1
+        
+        purge_list = []
+        for n,c in node_ids.items():
+            if c == 0:
+                purge_list.append(n)
+        c = db.cursor()
+        for i in range(0,len(purge_list),100):
+            query = "DELETE from nodes WHERE id in ('%s')" % "','".join(purge_list[i:i+100])
+            c.execute(query)
+        db.conn.commit()
+        c.close()
+        print "Deleted %s nodes of %d" % (len(purge_list), len(node_ids))
+        
+
+class PurgeDisjunctGraphsFilter(OSMDBFilter):
+    def filter(self, db, *args):
+        f = FindDisjunctGraphsFilter()
+        try:
+            f.teardown(db)
+        except: pass
+        
+        f.run(db,*args)
+        
+        largest = next(db.execute("SELECT graph_num, count(*) as cnt FROM graph_nodes GROUP BY graph_num ORDER BY cnt desc"))[0]
+        
+        node_ids = {}
+        
+        for x in db.execute("SELECT node_id FROM graph_nodes where graph_num != ?", (largest,)):
+            node_ids[x[0]] = 1
+
+        c = db.cursor()
+
+        purge_list = []
+        for way in db.ways():
+            if way.nds[0] in node_ids or way.nds[-1] in node_ids:
+                purge_list.append(way.id)
+
+        for i in range(0,len(purge_list),100):
+            query = "DELETE from ways WHERE id in ('%s')" % "','".join(purge_list[i:i+100])
+            c.execute(query)
+        db.conn.commit()
+        c.close()
+        print "Deleted %s ways" % (len(purge_list))
+        DeleteOrphanNodesFilter().run(db,*args)
+                
+        f.teardown(db)
+
 class FindDisjunctGraphsFilter(OSMDBFilter):
-    def setup(self, db):
+    def setup(self, db, *args):
         c = db.cursor()
         c.execute("CREATE table graph_nodes (graph_num INTEGER, node_id TEXT)")
         c.execute("CREATE index graph_nodes_node_indx ON graph_nodes(node_id)")
@@ -35,7 +109,7 @@ class FindDisjunctGraphsFilter(OSMDBFilter):
         c.execute("DROP table graph_nodes")
         c.close()
         
-    def filter(self, osmdb):
+    def filter(self, osmdb, *args):
         g = Graph()
         t0 = time.time()
         
@@ -76,9 +150,7 @@ class FindDisjunctGraphsFilter(OSMDBFilter):
         for gnum, count in osmdb.execute("SELECT graph_num, count(*) FROM graph_nodes GROUP BY graph_num"):
             print "FOUND: %s=%s" % (gnum, count)
         
-    def visualize(self, db, extra_args):
-        assert len(extra_args) == 1
-        filename = extra_args[0]
+    def visualize(self, db, out_filename):
         
         from prender import processing
         c = db.conn.cursor()
@@ -125,7 +197,7 @@ class FindDisjunctGraphsFilter(OSMDBFilter):
             mr.stroke(*color)                            
             mr.line(g[0][0],g[0][1],g[-1][0],g[-1][1])
             
-        mr.saveLocal(filename)
+        mr.saveLocal(out_filename)
         mr.stop()
         print "Done"
         
@@ -133,20 +205,26 @@ class FindDisjunctGraphsFilter(OSMDBFilter):
         
 def main():
     from sys import argv
-    mode, osmdb_file = argv[1:3]
-    db = OSMDB(osmdb_file)
+    filter_cls, mode, osmdb_file = argv[1:4]
     
-    f = FindDisjunctGraphsFilter()
+    try:
+        f = globals()[filter_cls]()
+    except KeyError, e:
+        raise Exception("Filter not found.")
+    
+    db = OSMDB(osmdb_file)
+
+    if len(argv) > 4:
+        extra = argv[4:]
+    else:
+        extra = []
+    
     if mode == 'run':
-        f.run(db)
+        f.run(db, *extra)
     elif mode == 'rerun':
-        f.rerun(db)
+        f.rerun(db, *extra)
     elif mode == 'visualize':
-        if len(argv) > 3:
-            extra = argv[3:]
-        else:
-            extra = []
-        f.visualize(db, extra)
+        f.visualize(db, *extra)
     else:
         raise Exception("Unknown mode.")
     
