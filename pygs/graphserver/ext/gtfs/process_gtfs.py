aac62f5fd8e82743c7e3df61a4ee230cb093a806
Nino Walker <kolohe@lscape.local>
2009-06-11 10:40:55 -0700

    Added a function for building GTFSDB to the gtfsdb file; used by setup.py.  process_gtfs.y will be obsolete, but left there for backward compatibility.

--- pygs/graphserver/ext/gtfs/process_gtfs.py
+++ pygs/graphserver/ext/gtfs/process_gtfs.py
@@ -1,14 +1,4 @@
-from gtfsdb import GTFSDatabase
+from gtfsdb import main_build_gtfsdb
 import sys
 
-if __name__=='__main__':
-  if len(sys.argv) < 3:
-    print "Converts GTFS file to GTFS-DB, which is super handy\nusage: python process_gtfs.py gtfs_filename, gtfsdb_filename"
-    exit()
-
-  gtfsdb_filename = sys.argv[2]
-  gtfs_filename = sys.argv[1]
- 
-  gtfsdb = GTFSDatabase( gtfsdb_filename, overwrite=True )
-  gtfsdb.load_gtfs( gtfs_filename, reporter=sys.stdout )
-
+if __name__=='__main__': main_build_gtfsb()
