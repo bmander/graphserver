293c271992459fa7564a6b9bc0db0c45fe01f731
Brandon Martin-Anderson <badhill@gmail.com>
2009-10-06 09:11:50 -0700

    gdb_import_gtfs actually imports a gtfs to a gdb

--- pygs/graphserver/compiler/gdb_import_gtfs.py
+++ pygs/graphserver/compiler/gdb_import_gtfs.py
@@ -5,57 +5,63 @@ from graphserver.ext.gtfs.gtfsdb import GTFSDatabase
 import sys
 from tools import gtfsdb_to_service_calendar
 
-def gdb_boardalight_load_bundle(gdb, agency_namespace, bundle, service_id, sc, tz, cursor):
-    stop_time_bundles = list(bundle.stop_time_bundles(service_id))
-#    n_trips = len(stop_time_bundles[0])
-#    
-#    
-#    # If there's less than two stations on this trip bundle, the trip bundle doesn't actually span two places
-#    if len(stop_time_bundles)<2:
-#        return
-#        
-#    # If there are no stop_times in a bundle on this service day, there is nothing to load
-#    if n_trips==0:
-#        return
-#        
-#    print "inserting %d trips with %d stop_time bundles on service_id '%s'"%(len(stop_time_bundles[0]),len(stop_time_bundles),service_id)
-#
-#    #add board edges
-#    for i, stop_time_bundle in enumerate(stop_time_bundles[:-1]):
-#        
-#        trip_id, departure_time, arrival_time, stop_id, stop_sequence, stop_dist_traveled = stop_time_bundle[0]
-#        
-#        patternstop_vx_name = "psv-%s-%03d-%03d"%(agency_namespace,bundle.pattern.pattern_id,i)
-#        
-#        #gdb.add_vertex( patternstop_vx_name, cursor )
-#        
-#        b = TripBoard(service_id, sc, tz, 0)
-#        for trip_id, departure_time, arrival_time, stop_id, stop_sequence, stop_dist_traveled in stop_time_bundle:
-#            b.add_boarding( trip_id, departure_time )
-#            
-#        #gdb.add_edge( "sta-%s"%stop_id, patternstop_vx_name, b, cursor )
-#        
-#    #add alight edges
-#    for i, stop_time_bundle in enumerate(stop_time_bundles[1:]):
-#
-#        trip_id, departure_time, arrival_time, stop_id, stop_sequence, stop_dist_traveled = stop_time_bundle[0]
-#        
-#        patternstop_vx_name = "psv-%s-%03d-%03d"%(agency_namespace,bundle.pattern.pattern_id,i+1)
-#        #gdb.add_vertex( patternstop_vx_name, cursor )
-#        
-#        al = Alight(service_id, sc, tz, 0)
-#        for trip_id, departure_time, arrival_time, stop_id, stop_sequence, stop_dist_traveled in stop_time_bundle:
-#            al.add_alighting( trip_id.encode('ascii'), arrival_time )
-#            
-#        #gdb.add_edge( patternstop_vx_name, "sta-%s"%stop_id, al, cursor )
-#    
-#    # add crossing edges
-#    for j, crossing_time in enumerate(bundle.pattern.crossings):
-#        c = Crossing( crossing_time )
-#        #gdb.add_edge( "psv-%s-%03d-%03d"%(agency_namespace,bundle.pattern.pattern_id,j), 
-#        #              "psv-%s-%03d-%03d"%(agency_namespace,bundle.pattern.pattern_id,j+1), c, cursor )
+def cons(ary):
+    for i in range(len(ary)-1):
+        yield (ary[i], ary[i+1])
 
-def gdb_load_gtfsdb_to_boardalight(gdb, agency_namespace, gtfsdb, agency_id, cursor, reporter=sys.stdout):
+def gdb_boardalight_load_bundle(gdb, agency_namespace, bundle, service_id, sc, tz, cursor):
+    stop_time_bundles = bundle.stop_time_bundles(service_id)
+    n_trips = len(bundle.trip_ids)    
+    
+    # If there's less than two stations on this trip bundle, the trip bundle doesn't actually span two places
+    if len(stop_time_bundles)<2:
+        return
+        
+    # If there are no stop_times in a bundle on this service day, there is nothing to load
+    if n_trips==0:
+        return
+        
+    print "inserting %d trips with %d stop_time bundles on service_id '%s'"%(len(stop_time_bundles[0]),len(stop_time_bundles),service_id)
+    #print bundle.pattern.stop_ids
+
+    #add board edges
+    for i, stop_time_bundle in enumerate(stop_time_bundles[:-1]):
+        
+        trip_id, departure_time, arrival_time, stop_id, stop_sequence, stop_dist_traveled = stop_time_bundle[0]
+        
+        patternstop_vx_name = "psv-%s-%03d-%03d"%(agency_namespace,bundle.pattern.pattern_id,i)
+        
+        gdb.add_vertex( patternstop_vx_name, cursor )
+        
+        b = TripBoard(service_id, sc, tz, 0)
+        for trip_id, departure_time, arrival_time, stop_id, stop_sequence, stop_dist_traveled in stop_time_bundle:
+            b.add_boarding( trip_id, departure_time )
+            
+        gdb.add_edge( "sta-%s"%stop_id, patternstop_vx_name, b, cursor )
+        
+    #add alight edges
+    for i, stop_time_bundle in enumerate(stop_time_bundles[1:]):
+
+        trip_id, departure_time, arrival_time, stop_id, stop_sequence, stop_dist_traveled = stop_time_bundle[0]
+        
+        patternstop_vx_name = "psv-%s-%03d-%03d"%(agency_namespace,bundle.pattern.pattern_id,i+1)
+        gdb.add_vertex( patternstop_vx_name, cursor )
+        
+        al = Alight(service_id, sc, tz, 0)
+        for trip_id, departure_time, arrival_time, stop_id, stop_sequence, stop_dist_traveled in stop_time_bundle:
+            al.add_alighting( trip_id.encode('ascii'), arrival_time )
+            
+        gdb.add_edge( patternstop_vx_name, "sta-%s"%stop_id, al, cursor )
+    
+    # add crossing edges
+    for j, crossing_time in enumerate(bundle.pattern.crossings):
+        c = Crossing( crossing_time )
+        gdb.add_edge( "psv-%s-%03d-%03d"%(agency_namespace,bundle.pattern.pattern_id,j), 
+                      "psv-%s-%03d-%03d"%(agency_namespace,bundle.pattern.pattern_id,j+1), c, cursor )
+                      
+    gdb.commit()
+
+def gdb_load_gtfsdb_to_boardalight(gdb, agency_namespace, gtfsdb, agency_id, cursor, maxtrips=None, reporter=sys.stdout):
 
     # get graphserver.core.Timezone and graphserver.core.ServiceCalendars from gtfsdb for agency with given agency_id
     tz = Timezone.generate(gtfsdb.agency_timezone_name( agency_id ))
@@ -69,13 +75,13 @@ def gdb_load_gtfsdb_to_boardalight(gdb, agency_namespace, gtfsdb, agency_id, cur
     
     # compile trip bundles from gtfsdb
     if reporter: reporter.write( "Compiling trip bundles...\n" )
-    bundles = gtfsdb.compile_trip_bundles(reporter=reporter)
+    bundles = gtfsdb.compile_trip_bundles(maxtrips=maxtrips, reporter=reporter)
 
     # load bundles to graph
     if reporter: reporter.write( "Loading trip bundles into graph...\n" )
     n_bundles = len(bundles)
     for i, bundle in enumerate(bundles):
-        if reporter and i%((n_bundles//n_bundles)+1)==0: reporter.write( "%d/%d trip bundles loaded\n"%(i, n_bundles) )
+        if reporter: reporter.write( "%d/%d loading %s\n"%(i, n_bundles, bundle) )
         
         for service_id in [x.encode("ascii") for x in gtfsdb.service_ids()]:
             gdb_boardalight_load_bundle(gdb, agency_namespace, bundle, service_id, sc, tz, cursor)
@@ -93,30 +99,30 @@ def gdb_load_gtfsdb_to_boardalight(gdb, agency_namespace, gtfsdb, agency_id, cur
         
         #add board edges
         for trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_dist_traveled in stoptimes[:-1]:
-            g.add_vertex( "hwv-%s-%s-%s"%(agency_namespace,stop_id, trip_id) )
-            g.add_edge( "sta-%s"%stop_id, "hwv-%s-%s-%s"%(agency_namespace,stop_id, trip_id), hb )
+            gdb.add_vertex( "hwv-%s-%s-%s"%(agency_namespace,stop_id, trip_id) )
+            gdb.add_edge( "sta-%s"%stop_id, "hwv-%s-%s-%s"%(agency_namespace,stop_id, trip_id), hb )
             
         #add alight edges
         for trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_dist_traveled in stoptimes[1:]:
-            g.add_vertex( "hwv-%s-%s-%s"%(agency_namespace,stop_id, trip_id) )
-            g.add_edge( "hwv-%s-%s-%s"%(agency_namespace,stop_id, trip_id), "sta-%s"%stop_id, ha )
-            print ha
+            gdb.add_vertex( "hwv-%s-%s-%s"%(agency_namespace,stop_id, trip_id) )
+            gdb.add_edge( "hwv-%s-%s-%s"%(agency_namespace,stop_id, trip_id), "sta-%s"%stop_id, ha )
         
         #add crossing edges
         for (trip_id1, arrival_time1, departure_time1, stop_id1, stop_sequence1, stop_dist_traveled1), (trip_id2, arrival_time2, departure_time2, stop_id2, stop_sequence2,stop_dist_traveled2) in cons(stoptimes):
-            g.add_edge( "hwv-%s-%s-%s"%(agency_namespace,stop_id1, trip_id1), "hwv-%s-%s-%s"%(agency_namespace,stop_id2, trip_id2), Crossing(arrival_time2-departure_time1) )
+            gdb.add_edge( "hwv-%s-%s-%s"%(agency_namespace,stop_id1, trip_id1), "hwv-%s-%s-%s"%(agency_namespace,stop_id2, trip_id2), Crossing(arrival_time2-departure_time1) )
             
     # load connections
     if reporter: reporter.write( "Loading connections to graph...\n" )
     for stop_id1, stop_id2, conn_type, distance in gtfsdb.execute( "SELECT * FROM connections" ):
-        g.add_edge( "sta-%s"%stop_id1, "sta-%s"%stop_id2, Street( conn_type, distance ) )
-        g.add_edge( "sta-%s"%stop_id2, "sta-%s"%stop_id1, Street( conn_type, distance ) )
+        gdb.add_edge( "sta-%s"%stop_id1, "sta-%s"%stop_id2, Street( conn_type, distance ) )
+        gdb.add_edge( "sta-%s"%stop_id2, "sta-%s"%stop_id1, Street( conn_type, distance ) )
         
 def main():
     usage = """usage: python gdb_import_gtfs.py [options] <graphdb_filename> <gtfsdb_filename> <agency_id>"""
     parser = OptionParser(usage=usage)
     parser.add_option("-n", "--namespace", dest="namespace", default="0",
                       help="agency namespace")
+    parser.add_option("-m", "--maxtrips", dest="maxtrips", default=None, help="maximum number of trips to load")
     
     (options, args) = parser.parse_args()
     
@@ -133,7 +139,8 @@ def main():
     gtfsdb = GTFSDatabase( gtfsdb_filename )
     gdb = GraphDatabase( graphdb_filename, overwrite=False )
     
-    gdb_load_gtfsdb_to_boardalight(gdb, options.namespace, gtfsdb, agency_id, gdb.get_cursor())
+    maxtrips = int(options.maxtrips) if options.maxtrips else None
+    gdb_load_gtfsdb_to_boardalight(gdb, options.namespace, gtfsdb, agency_id, gdb.get_cursor(), maxtrips=maxtrips)
     gdb.commit()
     
     print "done"
