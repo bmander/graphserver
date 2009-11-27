7f6b2522c54756b3d2055dbbdbe609f0083a5dee
Andrew Byrd <andrew@fastmail.net>
2009-11-27 15:32:34 +0100

    FindDisjunctGraphsFilter in osmfilters.py modified to work on sub-way edges instead of ways. Geometry information is included for visualizing disjunct graphs in a GIS.

--- pygs/graphserver/ext/osm/osmfilters.py
+++ pygs/graphserver/ext/osm/osmfilters.py
@@ -186,13 +186,20 @@ class StripOtherTagsFilter(OSMDBFilter):
 class FindDisjunctGraphsFilter(OSMDBFilter):
     def setup(self, db, *args):
         c = db.cursor()
-        c.execute("CREATE table graph_nodes (graph_num INTEGER, node_id TEXT)")
-        c.execute("CREATE index graph_nodes_node_indx ON graph_nodes(node_id)")
+        c.execute("DROP table if exists graph_nodes")
+        c.execute("DROP table if exists graph_edges")
+        c.execute("CREATE table graph_nodes (graph_num INTEGER, node_id TEXT, WKT_GEOMETRY TEXT)")
+        c.execute("CREATE table graph_edges (graph_num INTEGER, edge_id TEXT, WKT_GEOMETRY TEXT)")
+        c.execute("CREATE index graph_nodes_id_indx ON graph_nodes(node_id)")
+        c.execute("CREATE index graph_edges_id_indx ON graph_edges(edge_id)")
+        db.conn.commit()
         c.close()
 
     def teardown(self, db):
         c = db.cursor()
-        c.execute("DROP table graph_nodes")
+        c.execute("DROP table if exists graph_nodes")
+        c.execute("DROP table if exists graph_edges")
+        db.conn.commit()
         c.close()
         
     def filter(self, osmdb, *args):
@@ -201,35 +208,51 @@ class FindDisjunctGraphsFilter(OSMDBFilter):
         
         vertices = {}
         print "load vertices into memory"
-        for row in osmdb.execute("SELECT id from nodes"):
+        for row in osmdb.execute("SELECT DISTINCT start_nd from edges"):
             g.add_vertex(str(row[0]))
             vertices[str(row[0])] = 0
 
-        print "load ways into memory"
-        for way in osmdb.ways():
-            g.add_edge(way.nds[0], way.nds[-1], Link())
-            g.add_edge(way.nds[-1], way.nds[0], Link())
+        for row in osmdb.execute("SELECT DISTINCT end_nd from edges"):
+            g.add_vertex(str(row[0]))
+            vertices[str(row[0])] = 0
 
+        print "load edges into memory"
+        for start_nd, end_nd in osmdb.execute("SELECT start_nd, end_nd from edges"):
+            g.add_edge(start_nd, end_nd, Link())
+            g.add_edge(end_nd, start_nd, Link())
+               
         t1 = time.time()
         print "populating graph took: %f"%(t1-t0)
         t0 = t1
         
+        print "Total number of vertices: ", len(vertices)
         iteration = 1
         c = osmdb.cursor()
         while True:
-            #c.execute("SELECT id from nodes where id not in (SELECT node_id from graph_nodes) LIMIT 1")
             try:
                 vertex, dummy = vertices.popitem()
             except:
                 break
             spt = g.shortest_path_tree(vertex, None, State(1,0))
+            print "Found shortest path tree with %d vertices." % spt.size
             for v in spt.vertices:
+                lat, lon = c.execute("SELECT lat, lon from nodes where id=?", (v.label, )).next()
+                c.execute("INSERT into graph_nodes VALUES (?, ?, ?)", (iteration, v.label, "POINT(%f %f)" % (lon, lat)))
+                #print v.label
                 vertices.pop(v.label, None)
-                c.execute("INSERT into graph_nodes VALUES (?, ?)", (iteration, v.label))
+                # should not be necessary, must be a relic from when i forgot to put in double edges
+                # g.remove_vertex(v.label, True, True)
+                #print v.label
+                for e in g.get_vertex(v.label).outgoing : # get edges from original graph, since spt is tree-like 
+                    lat1, lon1 = c.execute("SELECT lat, lon from nodes where id=?", (e.from_v.label, )).next()
+                    lat2, lon2 = c.execute("SELECT lat, lon from nodes where id=?", (e.to_v.label, )).next()
+                    if lat1 <= lat2 : # include only one edge of each pair, since ways are not directed. if they are at exactly the same lat, you will get 2.
+                        c.execute("INSERT into graph_edges VALUES (?, ?, ?)", 
+                                (iteration, e.from_v.label + '->' + e.to_v.label, "LINESTRING(%f %f, %f %f)" % (lon1, lat1, lon2, lat2)))
             spt.destroy()
             
             t1 = time.time()
-            print "pass %s took: %f"%(iteration, t1-t0)
+            print "pass %s took: %f vertices remaining: %d"%(iteration, t1-t0, len(vertices))
             t0 = t1
             iteration += 1
         c.close()
