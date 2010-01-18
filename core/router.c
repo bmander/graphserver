5b82495defec198b33b995c9bcb9b75bd6b6edbe
Andrew Byrd <andrew@fastmail.net>
2010-01-18 22:42:00 +0100

    Vertices are copied to SPT only when they have a state. States are not even saved when they will exceed specified walk and transfer limits. closes #7

--- core/router.c
+++ core/router.c
@@ -52,22 +52,19 @@ gShortestPathTreeRetro( Graph* this, char *from, char *to, State* init_state, Wa
         u = gGetVertex( this, spt_u->label );
         // DEBUG
         // printf("got state. %s %ld\n", spt_u->label, du->time);
-        
+
+        // Abandon paths that exceed time limit.
+        // Break is appropriate here because all subsequent states will have greater times.
+        // We break out of the outer (queue) loop, not the inner (edge) loop.
 #ifndef RETRO
         // DEBUG
         maxtime = init_state->time + 60 * 60 * 2;
-
         if( du->time > maxtime )
             break;
 #else
         if( du->time < mintime )
             break;
 #endif
-        if( du->dist_walked > 2000 || du->num_transfers > 5 ) {
-            // DEBUG
-            // printf("excessive path length. abandoning.\n");
-            continue;
-        }
         
 #ifndef RETRO
         ListNode* edges = vGetOutgoingEdgeList( u );
@@ -85,12 +82,7 @@ gShortestPathTreeRetro( Graph* this, char *from, char *to, State* init_state, Wa
             v = edge->from;
 #endif
             spt_v = gGetVertex( spt, v->label );     //get the SPT Vertex corresponding to 'v'
-            if( !spt_v ) {                           // If this is the first time v has been reached
-                spt_v = gAddVertex( spt, v->label ); //Copy v over to the SPT
-                count++;
-                // DEBUG
-                // printf("Added vertex to spt. %s\n", spt_v->label);
-            }
+                                                     //might be null if it has not been copied!
 #ifndef RETRO
             State *new_dv = eWalk( edge, du, options );
 #else
@@ -108,57 +100,76 @@ gShortestPathTreeRetro( Graph* this, char *from, char *to, State* init_state, Wa
                 edges = edges->next;
                 continue;
             }
-            
-            dv = spt_v->payload;
-            State* prev_dv = NULL;
-            while ( dv ) {
+
+            // Abandon paths that exceed some limits (optimization)
+            // here we want to continue, not break, because it is possible to have later states with lower distance, etc.
+            if( new_dv->dist_walked > options->max_walk || new_dv->num_transfers > 3 ) {
                 // DEBUG
-                // printf("Comp State: %ld %ld\n", dv->time, dv->weight);
-                
-                //if (new_dv->weight >= dv->weight && new_dv->time >= dv->time) { // old is better in all respects
-                if (new_dv->weight >= dv->weight && new_dv->time >= dv->time && new_dv->num_transfers >= dv->num_transfers && new_dv->dist_walked >= dv->dist_walked) { // new is better in all respects
+                // printf("excessive path length. abandoning.\n");
+                stateDestroy(new_dv);
+                edges = edges->next;
+                continue;
+            }
+            
+            
+            if (spt_v) {
+                dv = spt_v->payload;
+                State* prev_dv = NULL;
+                while ( dv ) {
                     // DEBUG
-                    // printf("New state is worse. Abandoning.\n");
-                    stateDestroy(new_dv); // new_dv will never be used; merge it with the infinite.
-                    new_dv = NULL;        // signal that new state should not be added to the list
-                    break;                // no need to keep searching - new state is worse than everything in the list.
-                }
-                // new state has already been discarded if it is equal in all respects to old.
-                // equality here ensures uniqueness of times and weights in a state list.
-                // if (new_dv->weight <= dv->weight && new_dv->time <= dv->time) { // new is better in all respects
-                if (new_dv->weight <= dv->weight && new_dv->time <= dv->time && new_dv->num_transfers <= dv->num_transfers && new_dv->dist_walked <= dv->dist_walked) { // new is better in all respects
-                    // DEBUG
-                    // printf("New state is better. Deleting old.\n");
-                    // remove the old state from the linked list
-                    if (prev_dv) { // old state is not the head
-                        // DEBUG 
-                        // printf("Old state is not head.\n");
-                        prev_dv->next = dv->next;
-                    } else { // old state is the head
+                    // printf("Comp State: %ld %ld\n", dv->time, dv->weight);
+                    
+                    //if (new_dv->weight >= dv->weight && new_dv->time >= dv->time) { // old is better in all respects
+                    if (new_dv->weight >= dv->weight && new_dv->time >= dv->time && new_dv->num_transfers >= dv->num_transfers && new_dv->dist_walked >= dv->dist_walked) { // new is better in all respects
                         // DEBUG
-                        // printf("Old state is head.\n");
-                        spt_v->payload = dv->next;
+                        // printf("New state is worse. Abandoning.\n");
+                        stateDestroy(new_dv); // new_dv will never be used; merge it with the infinite.
+                        new_dv = NULL;        // signal that new state should not be added to the list
+                        break;                // no need to keep searching - new state is worse than everything in the list.
                     }
-                    // DEBUG
-                    // printf("Dequeueing.\n");
-                    if (dv->queue_node) fibheap_delete_node(q, dv->queue_node); // dequeue the old state.
-                    State* temp = dv;
-                    // prev_dv = prev_dv; // because current dv has been deleted
-                    dv = dv->next;
-                    // DEBUG
-                    // printf("Destroying.\n");
-                    stateDestroy(temp); // deallocate the old state
-                } else {
-                    prev_dv = dv;
-                    dv = dv->next;
+                    // new state has already been discarded if it is equal in all respects to old.
+                    // equality here ensures uniqueness of times and weights in a state list.
+                    // if (new_dv->weight <= dv->weight && new_dv->time <= dv->time) { // new is better in all respects
+                    if (new_dv->weight <= dv->weight && new_dv->time <= dv->time && new_dv->num_transfers <= dv->num_transfers && new_dv->dist_walked <= dv->dist_walked) { // new is better in all respects
+                        // DEBUG
+                        // printf("New state is better. Deleting old.\n");
+                        // remove the old state from the linked list
+                        if (prev_dv) { // old state is not the head
+                            // DEBUG 
+                            // printf("Old state is not head.\n");
+                            prev_dv->next = dv->next;
+                        } else { // old state is the head
+                            // DEBUG
+                            // printf("Old state is head.\n");
+                            spt_v->payload = dv->next;
+                        }
+                        // DEBUG
+                        // printf("Dequeueing.\n");
+                        if (dv->queue_node) fibheap_delete_node(q, dv->queue_node); // dequeue the old state.
+                        State* temp = dv;
+                        // prev_dv = prev_dv; // noop: because current dv has been deleted
+                        dv = dv->next;
+                        // DEBUG
+                        // printf("Destroying.\n");
+                        stateDestroy(temp); // deallocate the old state
+                    } else {
+                        prev_dv = dv;
+                        dv = dv->next;
+                    }
                 }
             }
             if (new_dv) { // check that we want to add the new state (that it has not been deleted)
+                if( !spt_v ) { // If this is the first time v has been reached
+                    spt_v = gAddVertex( spt, v->label ); // Copy v over to the SPT
+                    count++;
+                    // DEBUG
+                    // printf("Added vertex to spt. %s\n", spt_v->label);
+                }                            
                 // DEBUG
                 // printf("Keeping new state. Adding to linked list.\n");
-                dv = spt_v->payload;
+                State* temp = spt_v->payload;
                 spt_v->payload = new_dv;
-                new_dv->next = dv;
+                new_dv->next = temp;
                 new_dv->owner = spt_v;
                 new_dv->back_edge = edge;
                 new_dv->back_state = du;
