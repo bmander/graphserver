c0315ae7e455caf4720e840ae0533cae0d2bf0d6
Brandon Martin-Anderson <badhill@gmail.com>
2010-03-02 12:01:34 -0800

    crossings work propertly for headways

--- pygs/graphserver/compiler/gdb_import_gtfs.py
+++ pygs/graphserver/compiler/gdb_import_gtfs.py
@@ -156,7 +156,9 @@ def gdb_load_gtfsdb_to_boardalight(gdb, agency_namespace, gtfsdb, cursor, agency
         
         #add crossing edges
         for (trip_id1, arrival_time1, departure_time1, stop_id1, stop_sequence1, stop_dist_traveled1), (trip_id2, arrival_time2, departure_time2, stop_id2, stop_sequence2,stop_dist_traveled2) in cons(stoptimes):
-            gdb.add_edge( "hwv-%s-%s-%s"%(agency_namespace,stop_id1, trip_id1), "hwv-%s-%s-%s"%(agency_namespace,stop_id2, trip_id2), Crossing(arrival_time2-departure_time1) )
+            cr = Crossing()
+            cr.add_crossing_time( trip_id1, (arrival_time2-departure_time1) )
+            gdb.add_edge( "hwv-%s-%s-%s"%(agency_namespace,stop_id1, trip_id1), "hwv-%s-%s-%s"%(agency_namespace,stop_id2, trip_id2), cr )
             
     # load connections
     if reporter: reporter.write( "Loading connections to graph...\n" )
