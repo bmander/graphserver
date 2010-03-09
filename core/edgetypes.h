7c87b0ac0ef1db24ffe0e69053de62912a1d5ae3
Brandon Martin-Anderson <brandon@brandon-laptop.(none)>
2010-03-08 17:17:21 -0800

    moved code for the street edgetype off to its own place

--- core/edgetypes.h
+++ core/edgetypes.h
@@ -295,61 +295,6 @@ elapseTimeWalkBack(EdgePayload* superthis, State* param, WalkOptions* options);
 long
 elapseTimeGetSeconds(ElapseTime* this);
 
-
-//---------------DECLARATIONS FOR STREET  CLASS---------------------
-
-typedef struct Street {
-   edgepayload_t type;
-   State* (*walk)(struct EdgePayload*, struct State*, struct WalkOptions*);
-   State* (*walkBack)(struct EdgePayload*, struct State*, struct WalkOptions*);
-    
-   char* name;
-   double length;
-   float rise;
-   float fall;
-   float slog;
-   long way;
-} Street;
-
-Street*
-streetNew(const char *name, double length);
-
-Street*
-streetNewElev(const char *name, double length, float rise, float fall);
-
-void
-streetDestroy(Street* tokill);
-
-inline State*
-streetWalk(EdgePayload* superthis, State* state, WalkOptions* options);
-
-inline State*
-streetWalkBack(EdgePayload* superthis, State* state, WalkOptions* options);
-
-char*
-streetGetName(Street* this);
-
-double
-streetGetLength(Street* this);
-
-float
-streetGetRise(Street* this);
-
-float
-streetGetFall(Street* this);
-
-void
-streetSetRise(Street* this, float rise) ;
-
-void
-streetSetFall(Street* this, float fall) ;
-
-long
-streetGetWay(Street* this);
-
-void
-streetSetWay(Street* this, long way);
-
 //---------------DECLARATIONS FOR TRIPBOARD CLASS------------------------------------------
 
 typedef struct TripBoard {
---
+++ core/edgetypes/street.c
@@ -0,0 +1,84 @@
+
+//STREET FUNCTIONS
+Street*
+streetNew(const char *name, double length) {
+  Street* ret = (Street*)malloc(sizeof(Street));
+  ret->type = PL_STREET;
+  ret->name = (char*)malloc((strlen(name)+1)*sizeof(char));
+  strcpy(ret->name, name);
+  ret->length = length;
+  ret->rise = 0;
+  ret->fall = 0;
+  ret->slog = 1;
+  ret->way = 0;
+    
+  //bind functions to methods
+  ret->walk = &streetWalk;
+  ret->walkBack = &streetWalkBack;
+
+  return ret;
+}
+
+Street*
+streetNewElev(const char *name, double length, float rise, float fall) {
+    Street* ret = streetNew( name, length );
+    ret->rise = rise;
+    ret->fall = fall;
+    return ret;
+}
+
+void
+streetDestroy(Street* tokill) {
+  free(tokill->name);
+  free(tokill);
+}
+
+char*
+streetGetName(Street* this) {
+    return this->name;
+}
+
+double
+streetGetLength(Street* this) {
+    return this->length;
+}
+
+float
+streetGetRise(Street* this) {
+    return this->rise;
+}
+
+void
+streetSetRise(Street* this, float rise) {
+    this->rise = rise;
+}
+
+float
+streetGetFall(Street* this) {
+    return this->fall;
+}
+
+void
+streetSetFall(Street* this, float fall) {
+    this->fall = fall;
+}
+
+float
+streetGetSlog(Street* this) {
+    return this->slog;
+}
+
+void
+streetSetSlog(Street* this, float slog) {
+    this->slog = slog;
+}
+
+long
+streetGetWay(Street* this) {
+    return this->way;   
+}
+
+void
+streetSetWay(Street* this, long way) {
+    this->way = way;
+}
