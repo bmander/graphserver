a0b76cbed6745d79dff3f49829644b42475d9afa
Brandon Martin-Anderson <brandon@brandon-laptop.(none)>
2010-03-08 17:48:57 -0800

    moved crossing and headwayalight edgetypes to their own files

---
+++ core/edgetypes/crossing.h
@@ -0,0 +1,39 @@
+
+//---------------DECLARATIONS FOR CROSSING CLASS-------------------------------------------
+
+typedef struct Crossing {
+    edgepayload_t type;
+    State* (*walk)(struct EdgePayload*, struct State*, struct WalkOptions*);
+    State* (*walkBack)(struct EdgePayload*, struct State*, struct WalkOptions*);
+    
+    int* crossing_times;
+    char** crossing_time_trip_ids;
+    int n;
+} Crossing;
+
+Crossing*
+crNew( );
+
+void
+crDestroy(Crossing* this);
+
+void
+crAddCrossingTime(Crossing* this, char* trip_id, int crossing_time);
+
+int
+crGetCrossingTime(Crossing* this, char* trip_id);
+
+char*
+crGetCrossingTimeTripIdByIndex(Crossing* this, int i);
+
+int
+crGetCrossingTimeByIndex(Crossing* this, int i);
+
+int
+crGetSize(Crossing* this);
+
+inline State*
+crWalk( EdgePayload* superthis, State* state, WalkOptions* options );
+
+inline State*
+crWalkBack( EdgePayload* superthis, State* state, WalkOptions* options );
