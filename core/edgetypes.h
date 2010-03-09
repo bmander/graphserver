91bec3b7ec8dcd3a4ba6c0970fcf0c44240893c7
Brandon Martin-Anderson <brandon@brandon-laptop.(none)>
2010-03-08 17:43:07 -0800

    moved headwayboard into its own files

--- core/edgetypes.h
+++ core/edgetypes.h
@@ -182,60 +182,6 @@ epWalk( EdgePayload* this, State* param, WalkOptions* options );
 State*
 epWalkBack( EdgePayload* this, State* param, WalkOptions* options );
 
-//---------------DECLARATIONS FOR HEADWAYBOARD CLASS---------------------------------------
-
-typedef struct HeadwayBoard {
-    edgepayload_t type;
-    State* (*walk)(struct EdgePayload*, struct State*, struct WalkOptions*);
-    State* (*walkBack)(struct EdgePayload*, struct State*, struct WalkOptions*);
-    
-    ServiceId service_id;
-    char* trip_id;
-    int start_time;
-    int end_time;
-    int headway_secs;
-    
-    ServiceCalendar* calendar;
-    Timezone* timezone;
-    int agency;
-} HeadwayBoard;
-
-HeadwayBoard*
-hbNew(  ServiceId service_id, ServiceCalendar* calendar, Timezone* timezone, int agency, char* trip_id, int start_time, int end_time, int headway_secs );
-
-void
-hbDestroy(HeadwayBoard* this);
-
-ServiceCalendar*
-hbGetCalendar( HeadwayBoard* this );
-
-Timezone*
-hbGetTimezone( HeadwayBoard* this );
-
-int
-hbGetAgency( HeadwayBoard* this );
-
-ServiceId
-hbGetServiceId( HeadwayBoard* this );
-
-char*
-hbGetTripId( HeadwayBoard* this );
-
-int
-hbGetStartTime( HeadwayBoard* this );
-
-int
-hbGetEndTime( HeadwayBoard* this );
-
-int
-hbGetHeadwaySecs( HeadwayBoard* this );
-
-inline State*
-hbWalk( EdgePayload* superthis, State* state, WalkOptions* options );
-
-inline State*
-hbWalkBack( EdgePayload* superthis, State* state, WalkOptions* options );
-
 //---------------DECLARATIONS FOR HEADWAYALIGHT CLASS---------------------------------------
 
 typedef struct HeadwayAlight {
