ff3d23b3dac2aee439779bc401bd0c41897cbf5b
Brandon Martin-Anderson <brandon@brandon-laptop.(none)>
2010-03-10 23:05:23 -0800

    moved custom edgetype to its own file

--- core/edgetypes.c
+++ core/edgetypes.c
@@ -10,6 +10,7 @@
 #include "edgetypes/headwayalight.h"
 #include "edgetypes/crossing.h"
 #include "edgetypes/alight.h"
+#include "edgetypes/custom.h"
 #include "math.h"
 #include <stdio.h>
 
@@ -291,62 +292,6 @@ epWalkBack( EdgePayload* this, State* state, WalkOptions* options ) {
   }
   
   return this->walkBack( this, state, options );
-}
-
-// CUSTOM Payload Functions
-
-PayloadMethods*
-defineCustomPayloadType(void (*destroy)(void*),
-						State* (*walk)(void*,State*,WalkOptions*),
-						State* (*walkback)(void*,State*,WalkOptions*)) {
-	PayloadMethods* this = (PayloadMethods*)malloc(sizeof(PayloadMethods));
-	this->destroy = destroy;
-	this->walk = walk;
-	this->walkBack = walkback;
-	return this;
-}
-
-void
-undefineCustomPayloadType( PayloadMethods* this ) {
-	free(this);
-}
-
-CustomPayload*
-cpNew( void* soul, PayloadMethods* methods ) {
-	CustomPayload* this = (CustomPayload*)malloc(sizeof(CustomPayload));
-	this->type = PL_EXTERNVALUE;
-	this->soul = soul;
-	this->methods = methods;
-	return this;
-}
-
-void
-cpDestroy( CustomPayload* this ) {
-	this->methods->destroy(this->soul);
-	free( this );
-}
-
-void*
-cpSoul( CustomPayload* this ) {
-	return this->soul;
-}
-
-PayloadMethods*
-cpMethods( CustomPayload* this ) {
-	return this->methods;
-}
-
-State*
-cpWalk(CustomPayload* this, State* state, WalkOptions* walkoptions) {
-	State* s = this->methods->walk(this->soul, state, walkoptions);
-	s->prev_edge = (EdgePayload*)this;
-	return s;
-}
-State*
-cpWalkBack(CustomPayload* this, State* state, WalkOptions* walkoptions) {
-	State* s = this->methods->walkBack(this->soul, state, walkoptions);
-	s->prev_edge = (EdgePayload*)this;
-	return s;
 }
 
 #undef ROUTE_REVERSE
--- core/edgetypes.h
+++ core/edgetypes.h
@@ -182,44 +182,5 @@ epWalk( EdgePayload* this, State* param, WalkOptions* options );
 State*
 epWalkBack( EdgePayload* this, State* param, WalkOptions* options );
 
-typedef struct PayloadMethods {
-	void (*destroy)(void*);
-	State* (*walk)(void*,State*,WalkOptions*);
-	State* (*walkBack)(void*,State*,WalkOptions*);
-	//char* (*to_str)(void*);
-} PayloadMethods;
-
-typedef struct CustomPayload {
-  edgepayload_t type;
-  void* soul;
-  PayloadMethods* methods;
-} CustomPayload;
-
-PayloadMethods*
-defineCustomPayloadType(void (*destroy)(void*),
-						State* (*walk)(void*,State*,WalkOptions*),
-						State* (*walkback)(void*,State*,WalkOptions*));
-
-
-void
-undefineCustomPayloadType( PayloadMethods* this );
-
-CustomPayload*
-cpNew( void* soul, PayloadMethods* methods );
-
-void
-cpDestroy( CustomPayload* this );
-
-void*
-cpSoul( CustomPayload* this );
-
-PayloadMethods*
-cpMethods( CustomPayload* this );
-
-State*
-cpWalk(CustomPayload* this, State* state, struct WalkOptions* walkoptions);
-
-State*
-cpWalkBack(CustomPayload* this, State* state, struct WalkOptions* walkoptions);
 
 #endif
