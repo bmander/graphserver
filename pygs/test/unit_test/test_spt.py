cd920184de86742610984b91928318dd723c2874
Brandon Martin-Anderson <badhill@gmail.com>
2010-07-17 00:49:32 -0700

    added unit tests for SPTVertex and SPT

--- pygs/test/unit_test/test_spt.py
+++ pygs/test/unit_test/test_spt.py
@@ -1,7 +1,8 @@
-from graphserver.core import Graph, Vertex, Edge, Street, State
+from graphserver.core import *
 import unittest
+import time
 
-class TestShortestPathTreePathRetroBasic(unittest.TestCase):
+class TestShortestPathTree(unittest.TestCase):
     def setUp(self):
         self.gg = Graph()
         self.A = self.gg.add_vertex( "A" )
@@ -19,5 +20,160 @@ class TestShortestPathTreePathRetroBasic(unittest.TestCase):
         self.assertEqual( vertices[1].label , self.A.label )
         self.assertEqual( edges[0].payload.name , self.a.payload.name )
         
+    def test_basic(self):
+        spt = ShortestPathTree()
+        assert spt
+        
+        spt.destroy()
+        
+    def test_empty_graph(self):
+        spt = ShortestPathTree()
+        assert spt.vertices == []
+        
+        spt.destroy()
+        
+    def test_add_vertex(self):
+        spt = ShortestPathTree()
+        v = spt.add_vertex("home")
+        assert v.label == "home"
+        
+        spt.destroy()
+        
+    def test_remove_vertex(self):
+        spt = ShortestPathTree()
+        spt.add_vertex( "A" )
+        spt.get_vertex( "A" ).label == "A"
+        spt.remove_vertex( "A" )
+        assert spt.get_vertex( "A" ) == None
+        
+        spt.add_vertex( "A" )
+        spt.add_vertex( "B" )
+        pl = Street( "AB", 1 )
+        spt.add_edge( "A", "B", pl )
+        spt.remove_vertex( "A" )
+        assert pl.name == "AB"
+        assert spt.get_vertex( "A" ) == None
+        assert spt.get_vertex( "B" ).label == "B"
+        
+    def test_add_vertices(self):
+        spt = ShortestPathTree()
+        verts = range(0,1000)
+        t0 = time.time()
+        spt.add_vertices(verts)
+        print "add vertices elapsed ", (time.time() - t0)
+        vlist = spt.vertices
+        assert len(vlist) == len(verts)
+        vlist.sort(lambda x, y: int(x.label) - int(y.label))
+        assert vlist[0].label == "0"
+        assert vlist[-1].label == str(verts[-1])
+        spt.destroy()
+        
+        
+    def test_double_add_vertex(self):
+        spt = ShortestPathTree()
+        v = spt.add_vertex("double")
+        assert v.label == "double"
+        assert spt.size == 1
+        v = spt.add_vertex("double")
+        assert spt.size == 1
+        assert v.label == "double"
+        
+        spt.destroy()
+        
+    def test_get_vertex(self):
+        spt = ShortestPathTree()
+        
+        spt.add_vertex("home")
+        v = spt.get_vertex("home")
+        assert v.label == "home"
+        v = spt.get_vertex("bogus")
+        assert v == None
+        
+        spt.destroy()
+        
+    def test_add_edge(self):
+        spt = ShortestPathTree()
+        
+        fromv = spt.add_vertex("home")
+        tov = spt.add_vertex("work")
+        s = Street( "helloworld", 1 )
+        e = spt.add_edge("home", "work", s)
+        assert e
+        assert e.from_v.label == "home"
+        assert e.to_v.label == "work"
+        assert str(e)=="<Edge><Street name='helloworld' length='1.000000' rise='0.000000' fall='0.000000' way='0'/></Edge>"
+        
+        spt.destroy()
+    
+    def test_add_edge_effects_vertices(self):
+        spt = ShortestPathTree()
+        
+        fromv = spt.add_vertex("home")
+        tov = spt.add_vertex("work")
+        s = Street( "helloworld", 1 )
+        e = spt.add_edge("home", "work", s)
+        
+        assert fromv.degree_out==1
+        assert tov.degree_in==1
+        
+        spt.destroy()
+    
+    def test_vertices(self):
+        spt = ShortestPathTree()
+        
+        fromv = spt.add_vertex("home")
+        tov = spt.add_vertex("work")
+        
+        assert spt.vertices
+        assert len(spt.vertices)==2
+        assert spt.vertices[0].label == 'home'
+        
+        spt.destroy()
+
+        
+    def test_walk_longstreet(self):
+        spt = ShortestPathTree()
+        
+        fromv = spt.add_vertex("home")
+        tov = spt.add_vertex("work")
+        s = Street( "helloworld", 24000 )
+        e = spt.add_edge("home", "work", s)
+        
+        wo = WalkOptions()
+        sprime = e.walk(State(spt.numagencies,0), wo)
+        wo.destroy()
+        print str(sprime)
+        assert str(sprime)=="<state time='3953' weight='5538153' dist_walked='24000.0' num_transfers='0' trip_id='None' stop_sequence='-1'></state>"
+
+        spt.destroy()
+    
+        
+    def test_add_link(self):
+        spt = ShortestPathTree()
+        
+        fromv = spt.add_vertex("home")
+        tov = spt.add_vertex("work")
+        s = Street( "helloworld", 1 )
+        e = spt.add_edge("home", "work", s)
+        
+        assert e.payload
+        assert e.payload.__class__ == Street
+        
+        x = spt.add_edge("work", "home", Link())
+        assert x.payload
+        assert x.payload.name == "LINK"
+        
+        spt.destroy()
+        
+    def test_edgeclass(self):
+        spt = ShortestPathTree()
+        spt.add_vertex( "A" )
+        spt.add_vertex( "B" )
+        spt.add_edge( "A", "B", Street("AB", 1) )
+        
+        vv = spt.get_vertex( "A" )
+        assert vv.__class__ == SPTVertex
+        assert vv.outgoing[0].__class__ == SPTEdge
+        assert vv.outgoing[0].to_v.__class__ == SPTVertex
 if __name__ == '__main__':
     unittest.main()
---
+++ pygs/test/unit_test/test_sptvertex.py
@@ -0,0 +1,43 @@
+import unittest
+from graphserver.core import *
+
+class TestSPTVertex(unittest.TestCase):
+    def test_basic(self):
+        v=SPTVertex("home")
+        assert v
+        
+    def test_destroy(self): #mostly just check that it doesn't segfault. the stress test will check if it works or not.
+        v=SPTVertex("home")
+        v.destroy()
+        
+        try:
+            v.label
+            assert False #pop exception by now
+        except:
+            pass
+        
+    def test_label(self):
+        v=SPTVertex("home")
+        print v.label
+        assert v.label == "home"
+    
+    def test_incoming(self):
+        v=SPTVertex("home")
+        assert v.incoming == []
+        assert v.degree_in == 0
+        
+    def test_outgoing(self):
+        v=SPTVertex("home")
+        assert v.outgoing == []
+        assert v.degree_out == 0
+        
+    def test_prettyprint(self):
+        v = SPTVertex("home")
+        assert v.to_xml() == "<SPTVertex degree_out='0' degree_in='0' label='home'/>"
+
+
+if __name__ == '__main__':
+    tl = unittest.TestLoader()
+
+    suite = tl.loadTestsFromTestCase(TestSPTVertex)
+    unittest.TextTestRunner(verbosity=2).run(suite)
