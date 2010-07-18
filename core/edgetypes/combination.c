d644505e1f051a07ca5717124c460f4e4414c4f0
Brandon Martin-Anderson <badhill@gmail.com>
2010-07-17 17:42:16 -0700

    added implementation of Combination edgetype

---
+++ core/edgetypes/combination.c
@@ -0,0 +1,44 @@
+#include "../graphserver.h"
+
+//=========COMBINATION FUNCTIONS==============
+
+Combination*
+comboNew(EdgePayload* first, EdgePayload* second) {
+    Combination* ret = (Combination*)malloc(sizeof(Combination));
+    ret->type = PL_COMBINATION;
+    ret->first = first;
+    ret->second = second;
+    return ret;
+}
+
+void
+comboDestroy(Combination* tokill) {
+    free( tokill );
+}
+
+inline State*
+comboWalk(EdgePayload* this, State* param, WalkOptions* options) {
+    State* intermediate = epWalk( ((Combination*)this)->first, param, options );
+    State* ret = epWalk( ((Combination*)this)->second, intermediate, options );
+    stateDestroy( intermediate );
+    return ret;
+}
+
+inline State*
+comboWalkBack(EdgePayload* this, State* param, WalkOptions* options) {
+    State* intermediate = epWalkBack( ((Combination*)this)->second, param, options );
+    State* ret = epWalkBack( ((Combination*)this)->first, intermediate, options );
+    stateDestroy( intermediate );
+    return ret;
+}
+
+EdgePayload*
+comboGetFirst(Combination* this) {
+    return this->first;
+}
+
+EdgePayload*
+comboGetSecond(Combination* this) {
+    return this->second;
+}
+
