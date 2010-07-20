be920f07c5cfe03f8fa81212f43856a36172c42d
Brandon Martin-Anderson <badhill@gmail.com>
2010-07-20 16:43:42 -0700

    added another unit test

--- core/test/test_ch.c
+++ core/test/test_ch.c
@@ -22,8 +22,31 @@ void test_empty_graph() {
 
 }
 
+void test_simple_graph() {
+  
+  // create a simple graph with two vertices and one edge
+  Graph *gg = gNew();
+  gAddVertex( gg, "A" );
+  gAddVertex( gg, "B" );
+  gAddEdge( gg, "A", "B", (EdgePayload*)streetNew( "AtoB", 10, 0 ) );
+
+  // create a contraction hierarchy of the graph
+  WalkOptions *wo = woNew();
+  CH *ch = get_contraction_hierarchies( gg, wo, 1 );
+
+  // asserts
+  assert( gSize( ch->up ) == 2 );
+  assert( gSize( ch->down ) == 2 );
+
+  // clean up
+  woDestroy( wo );
+  chDestroy( ch );
+  gDestroy( gg );
+}
+
 int main() {
   test_empty_graph();
+  test_simple_graph();
 
   return 1;
 }
