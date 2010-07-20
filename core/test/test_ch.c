3a6893f4b0cb4cdd705e9aecaa431f239d795f1a
Brandon Martin-Anderson <badhill@gmail.com>
2010-07-20 15:56:06 -0700

    add unit test

--- core/test/test_ch.c
+++ core/test/test_ch.c
@@ -1,10 +1,29 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "../graphserver.h"
+#include "../graph.h"
+#include "../heap.h"
+#include "../contraction.h"
 #include <string.h>
 
+void test_empty_graph() {
+
+  Graph *gg = gNew();
+  WalkOptions *wo = woNew();
+
+  CH *ch = get_contraction_hierarchies( gg, wo, 1 ); 
+
+  assert( gSize( ch->up ) == 0 );
+  assert( gSize( ch->down ) == 0 );
+
+  woDestroy( wo );
+  chDestroy( ch );
+  gDestroy( gg );
+
+}
 
 int main() {
+  test_empty_graph();
 
   return 1;
 }
